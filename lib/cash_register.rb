3558edee93f673f397111e03e878fecd6e2ad7ad
Marie Burns <mcburns2222@gmail.com>
2018-12-09 00:32:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+  attr_accessor :total
+  
+  
+  def initialize
+   @total = 0 
+   self.apply_discount
+end
 
+def total
+  @total
+end
+
+def add_item(item, price, quantity=1)
+  @total += (price * quantity)
+end
+
+def apply_discount
+  @total 
+end
+
+def void_last_transaction
+end
+
+end
